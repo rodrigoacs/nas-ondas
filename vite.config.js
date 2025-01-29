7d38a067a7c84a5402a836bd6aec72debcd1a828
rodrigoacs <rodrigohths@gmail.com>
2025-01-29 15:47:39 -0300

    chore: vite build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    sourcemap: false,
+    cssCodeSplit: true,
+  },
   plugins: [
     vue(),
     vueDevTools(),
